4e9fd6c86f2f6a3b9d9409b8398654987b0591bc
Patrick Meyers <pmeyers@caltech.edu>
2024-01-16 11:38:44 -0800

    update notebooks.

--- modules/compute_omega.py
+++ modules/compute_omega.py
@@ -15,8 +15,14 @@ import gwBackground_module as gwb
 
 import astropy.units as u
 
+
+def calculate_total_rate(zs, p_dz):
+    p_dz_centers = (p_dz[1:] + p_dz[:-1]) / 2.0
+    return np.sum(np.diff(zs) * p_dz_centers)
+
+
 def calculate_num_injections(T_obs, zs, p_dz):
-    '''
+    """
     Calculate the number of injections for the observation time.
 
     Parameters
@@ -31,10 +37,9 @@ def calculate_num_injections(T_obs, zs, p_dz):
     -------
     double
         theoretical number of events for the observation time
-    '''
-    p_dz_centers = (p_dz[1:] + p_dz[:-1]) / 2.
-    total_sum = np.sum(np.diff(zs) * p_dz_centers)
-    return T_obs * total_sum
+    """
+    total_rate = calculate_total_rate(zs, p_dz)
+    return T_obs * total_rate
 
 
 def draw_injections(prior_dict, Tobs):
@@ -54,18 +59,24 @@ def draw_injections(prior_dict, Tobs):
     injections : dict
         dictionary containing information on injections
     """
-    zs, p_dz = prior_dict['redshift']._get_redshift_arrays()
+    zs, p_dz = prior_dict["redshift"]._get_redshift_arrays()
     N = calculate_num_injections(Tobs, zs, p_dz)
 
     logger.info(f"We are averaging over {N} waveforms for {np.round(Tobs, 3)} years")
     N_inj = np.random.poisson(N.value)
     injections = prior_dict.sample(N_inj)
-    injections["signal_type"] = 'CBC'
+    injections["signal_type"] = "CBC"
     return injections
 
 
-def sample_dict_compute_injected_omega(prior_dict, Tobs, duration=10, f_ref=25, sampling_frequency=2048,
-                                       approximant='IMRPhenomD'):
+def sample_dict_compute_injected_omega(
+    prior_dict,
+    Tobs,
+    duration=10,
+    f_ref=25,
+    sampling_frequency=2048,
+    approximant="IMRPhenomD",
+):
     # Calculate number of injections
     injections = draw_injections(prior_dict, Tobs)
 
@@ -78,8 +89,8 @@ def sample_dict_compute_injected_omega(prior_dict, Tobs, duration=10, f_ref=25, 
         waveform_arguments={
             "waveform_approximant": approximant,
             "reference_frequency": 50,
-            "minimum_frequency": 1
-        }
+            "minimum_frequency": 1,
+        },
     )  # breaks down at M_tot * (1+z) ~ 1000, according to Xiao-Xiao and Haowen
 
     # convert to seconds, set up frequency array for waveform
@@ -88,22 +99,21 @@ def sample_dict_compute_injected_omega(prior_dict, Tobs, duration=10, f_ref=25, 
     omega_gw_freq = np.zeros(len(freqs_psd))
 
     try:
-        N_inj = len(injections['geocent_time']['content'])
+        N_inj = len(injections["geocent_time"]["content"])
     except:
-        N_inj = len(injections['geocent_time'])
+        N_inj = len(injections["geocent_time"])
 
-    logger.info('Compute the total injected Omega for ' + str(N_inj) + ' injections')
+    logger.info("Compute the total injected Omega for " + str(N_inj) + " injections")
 
     # Loop over injections
     for i in tqdm(range(N_inj)):
-
         inj_params = {}
         # Generate the individual parameters dictionary for each injection
         for k in injections.keys():
-            if k == 'signal_type':
+            if k == "signal_type":
                 continue
             try:
-                inj_params[k] = injections[k]['content'][i]
+                inj_params[k] = injections[k]["content"][i]
             except:
                 inj_params[k] = injections[k][i]
 
@@ -111,10 +121,10 @@ def sample_dict_compute_injected_omega(prior_dict, Tobs, duration=10, f_ref=25, 
         polarizations = waveform_generator.frequency_domain_strain(inj_params)
 
         # Final PSD of the injection
-        psd = np.abs(polarizations['plus']) ** 2 + np.abs(polarizations['cross']) ** 2
+        psd = np.abs(polarizations["plus"]) ** 2 + np.abs(polarizations["cross"]) ** 2
 
         # Add to Omega_spectrum
-        omega_gw_freq += 2 * np.pi ** 2 * freqs_psd ** 3 * psd / (3 * H0.si.value ** 2)
+        omega_gw_freq += 2 * np.pi**2 * freqs_psd**3 * psd / (3 * H0.si.value**2)
 
     Tobs_seconds = Tobs * 86400 * 365.25  # years to seconds
     omega_gw_freq *= 2 / Tobs_seconds
@@ -130,25 +140,27 @@ def calculate_omega_gridded(prior_dict, fref=25):
     zs = np.linspace(0, 10, num=1000)
 
     inspiralOnly = False
-    m1_min = prior_dict['mass_1'].minimum
-    m1_max = prior_dict['mass_1'].maximum
-    m2_min = prior_dict['mass_2'].minimum
-    m2_max = prior_dict['mass_2'].maximum
-    minimum_component_mass = prior_dict['mass_2'].minimum
-    maximum_component_mass = prior_dict['mass_1'].maximum
+    m1_min = prior_dict["mass_1"].minimum
+    m1_max = prior_dict["mass_1"].maximum
+    m2_min = prior_dict["mass_2"].minimum
+    m2_max = prior_dict["mass_2"].maximum
+    minimum_component_mass = prior_dict["mass_2"].minimum
+    maximum_component_mass = prior_dict["mass_1"].maximum
     omg = gwb.OmegaGW_BBH(minimum_component_mass, maximum_component_mass, zs)
 
     # Calculate merger rate
-    mergerRate = prior_dict['redshift']._get_rate_vs_redshift(zs)
+    mergerRate = prior_dict["redshift"]._get_rate_vs_redshift(zs)
 
     # Calculate probability grid
     # Priors defined in (m1, q) space
     probs = np.empty((omg.m1s_2d.shape[0], omg.qs_2d.shape[1]))  # initialize array
     for i in range(omg.m1s_2d.shape[0]):  # for each m1
         for j in range(omg.qs_2d.shape[1]):  # for each q
-            prob = prior_dict.prob({'mass_1': omg.m1s_2d[i][j], 'mass_ratio': omg.qs_2d[i][j]})  # calculate probability
+            prob = prior_dict.prob(
+                {"mass_1": omg.m1s_2d[i][j], "mass_ratio": omg.qs_2d[i][j]}
+            )  # calculate probability
             probs[i][j] = prob  # insert probability into probability array
-    probs = probs * (omg.Mtots_2d / (1. + omg.qs_2d))  # multiply by the Jacobian
+    probs = probs * (omg.Mtots_2d / (1.0 + omg.qs_2d))  # multiply by the Jacobian
 
     # Limit probabilities to physical masses
     probs[omg.m1s_2d < m1_min] = 0
@@ -162,13 +174,13 @@ def calculate_omega_gridded(prior_dict, fref=25):
 
     # Calculate Omega(f)
     freqs_TC = np.logspace(0, np.log10(1000), 200)
-    OmegaGW_TC = omg.eval(prior_dict['redshift'].R0.value, mergerRate, freqs_TC)
+    OmegaGW_TC = omg.eval(prior_dict["redshift"].R0.value, mergerRate, freqs_TC)
 
     # Find OmegaGW(fref)
     fref_approx_TC = 100
     fref_approx_index_TC = 0
     for i in range(len(freqs_TC)):
-        if (np.absolute(freqs_TC[i] - fref) < fref_approx_TC):
+        if np.absolute(freqs_TC[i] - fref) < fref_approx_TC:
             fref_approx_TC = np.absolute(freqs_TC[i] - fref)
             fref_approx_index_TC = i
 
